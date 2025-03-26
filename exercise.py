46b169fa894fa6c1cd8cc1bc388de87e966893ae
Madhu <madhusreenulu@gmail.com>
2025-03-26 20:01:30 +0530

    Trying

---
+++ exercise.py
@@ -0,0 +1,12 @@
+Initialize
+connecting ur local project to github 
+    git remote add origin url
+check remote url
+    git remote -v
+add files to git
+    git add .
+commit changes
+    git commit -m "Your message"
+push to github
+    git push -u origin main
+Refresh the page and check
